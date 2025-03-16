525a392433d3367e2f7c865d43d6634af8f59e49
Nugulk <Nugulk@DESKTOP-LQ4A0A4>
2025-03-16 17:41:43 +0900

    1

---
+++ HelloWorld/src/com/yedam/bookApp/User.java
@@ -0,0 +1,14 @@
+package com.yedam.bookApp;
+
+public class User {
+	private String userId;
+	private String userName;
+	private String password;
+
+	public User(String userId, String userName, String password) {
+		this.userId = userId;
+		this.userName = userName;
+		this.password = password;
+		
+	}
+}
